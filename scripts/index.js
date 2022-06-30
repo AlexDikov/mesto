cf2a36ecdeddb8ca4af1c821e53e5a4c4457ebd2
Alex Dikov <dikov_alex@mail.ru>
2022-06-30 22:18:03 +0300

    second_check

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,35 @@
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
-const saveButton = popup.querySelector('.popup__save-button');
-const nameInput = popup.querySelector('.popup__name-form');
-const jobInput = popup.querySelector('.popup__job-form');
 const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_field_name');
+const jobInput = formElement.querySelector('.popup__input_field_job');
 const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const likeButton = document.querySelector('.element-place__like-button');
 
 function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+
   popup.classList.add('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-
 function closePopup() {
-  popup.classList.remove('popup_opened');
-  
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-}
 
-closeButton.addEventListener('click', closePopup);
-
-function saveChanges() {
   popup.classList.remove('popup_opened');
 }
-formElement.addEventListener('submit', saveChanges);
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+
+  closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
